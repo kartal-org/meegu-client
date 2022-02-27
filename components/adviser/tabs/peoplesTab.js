bfe396ae7200ba76c740be3c2685b39dc747ec58
Thania Sinogaya <thania.sinogaya@gmail.com>
2022-02-27 20:55:34 +0800

    packages

--- components/adviser/tabs/peoplesTab.js
+++ components/adviser/tabs/peoplesTab.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import PageLayout from '../../../layouts/pageLayout';
-import PeopleCard from '../../reusable/peopleCard';
-import styles from './tabs.module.scss';
-import Autocomplete from '@mui/material/Autocomplete';
-import fileImg from '../../../public/Files.png';
-import Cookies from 'js-cookie';
-import { Button, TextField } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import CustomizedDialogs from '../../reusable/dialog2';
-import { Box } from '@mui/system';
+import React, { useEffect, useState } from "react";
+import PageLayout from "../../../layouts/pageLayout";
+import PeopleCard from "../../reusable/peopleCard";
+import styles from "./tabs.module.scss";
+import Autocomplete from "@mui/material/Autocomplete";
+import fileImg from "../../../public/Files.png";
+import Cookies from "js-cookie";
+import { Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import CustomizedDialogs from "../../reusable/dialog2";
+import { Box } from "@mui/system";
 
 function PeoplesTab({ institutionID }) {
 	const [membersList, setMembersList] = useState([]);
@@ -22,28 +22,31 @@ function PeoplesTab({ institutionID }) {
 			? `/institutions/members?institution=${institutionID}&type=${type}`
 			: `/institutions/members?institution=${institutionID}`;
 
-		queryLink = queryLink + (isNotActive ? `&isNotActive=${true}` : '');
+		queryLink = queryLink + (isNotActive ? `&isNotActive=${true}` : "");
 
 		console.log(queryLink);
 
-		const responseGetMembers = await fetch(process.env.BACKEND_API_UR + queryLink, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Cookies.get('access_token')}`,
-			},
-		});
+		const responseGetMembers = await fetch(
+			process.env.BACKEND_API_UR + queryLink,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${Cookies.get("access_token")}`,
+				},
+			}
+		);
 		const resultMember = await responseGetMembers.json();
-		console.log('members', resultMember);
+		console.log("members", resultMember);
 		setMembersList(resultMember);
 	}
 
 	async function fetchPeople(type) {
-		let queryLink = type ? `/users?type=${type}` : '/users';
+		let queryLink = type ? `/users?type=${type}` : "/users";
 		const response = await fetch(process.env.BACKEND_API_UR + queryLink, {
 			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Cookies.get('access_token')}`,
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${Cookies.get("access_token")}`,
 			},
 		});
 		const result = await response.json();
@@ -59,16 +62,27 @@ function PeoplesTab({ institutionID }) {
 		const getUser = userList.filter(
 			(val) => `${val.first_name} ${val.last_name}` === selectedUser
 		);
-		console.log({ user: getUser[0].id, institution: institutionID, isActive: true });
-
-		const request = await fetch(process.env.BACKEND_API_UR + '/institutions/members', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Cookies.get('access_token')}`,
-			},
-			body: JSON.stringify({ user: getUser[0].id, institution: institutionID, isActive: true }),
+		console.log({
+			user: getUser[0].id,
+			institution: institutionID,
+			isActive: true,
 		});
+
+		const request = await fetch(
+			process.env.BACKEND_API_UR + "/institutions/members",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${Cookies.get("access_token")}`,
+				},
+				body: JSON.stringify({
+					user: getUser[0].id,
+					institution: institutionID,
+					isActive: true,
+				}),
+			}
+		);
 		const result = await request.json();
 		console.log(result);
 		setMembersList([result, ...membersList]);
@@ -80,9 +94,9 @@ function PeoplesTab({ institutionID }) {
 	}, []);
 	return (
 		<>
-			<div>
+			<div className={styles.addPeopleBtn}>
 				<CustomizedDialogs
-					title='Add People'
+					title="Add People"
 					openBtn={<Button>Add People</Button>}
 					primaryAction={<Button onClick={handleSubmit(addPeople)}>Add</Button>}
 				>
@@ -95,22 +109,31 @@ function PeoplesTab({ institutionID }) {
 							}}
 							// value={selectedUser}
 							options={userList?.filter((array) =>
-								membersList.map((val) => val.user).some((filter) => filter.id !== array.id)
+								membersList
+									.map((val) => val.user)
+									.some((filter) => filter.id !== array.id)
 							)}
-							getOptionLabel={(option) => option.first_name + ' ' + option.last_name}
+							getOptionLabel={(option) =>
+								option.first_name + " " + option.last_name
+							}
 							fullWidth
-							{...register('selectedUser')}
+							{...register("selectedUser")}
 							renderInput={(params) => (
-								<TextField fullWidth {...params} label='Search User' />
+								<TextField fullWidth {...params} label="Search User" />
 							)}
 							renderOption={(props, option) => (
 								<Box
 									onClick={(event, option) => console.log(option)}
-									component='li'
-									sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+									component="li"
+									sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
 									{...props}
 								>
-									<img loading='lazy' width='20' src={option.profileImage} alt='' />
+									<img
+										loading="lazy"
+										width="20"
+										src={option.profileImage}
+										alt=""
+									/>
 									{option.first_name} {option.last_name}
 								</Box>
 							)}
