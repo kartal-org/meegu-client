037cef268f8cf6f2efd698b2b7c71b3dc480acd1
Thania Sinogaya <thania.sinogaya@gmail.com>
2022-02-28 12:43:05 +0800

    register user

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,15 @@
-import { NextSeo } from 'next-seo';
-import { useUser, useUserUpdate } from '../contexts/userProvider';
-import { withAuthMedia, genericReq } from '../axios/axiosInstances';
-import AuthLayout from '../layouts/authLayout';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import { NextSeo } from "next-seo";
+import { useUser, useUserUpdate } from "../contexts/userProvider";
+import { withAuthMedia, genericReq } from "../axios/axiosInstances";
+import AuthLayout from "../layouts/authLayout";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import styles from "../styles/register.module.scss";
+import Image from "next/image";
+
+import adviser from "../public/student.png";
+import moderator from "../public/principal.png";
+import researcher from "../public/student-img.png";
 
 // This page will edit the user type of the new authenticated user.
 
@@ -16,28 +22,43 @@ function Register() {
 	const userTypes = [
 		{
 			id: 1,
-			user: 'Researcher',
+			user: "Researcher",
 			img: (
 				<>
-					<img src='https://cdn-icons.flaticon.com/png/128/3153/premium/3153024.png?token=exp=1645165396~hmac=1051abdc078cbadfe20b94aa3e7dc0d0' />
+					<Image
+						src={researcher}
+						layout="fill"
+						objectFit="contain"
+						className={styles.illustration}
+					></Image>
 				</>
 			),
 		},
 		{
 			id: 2,
-			user: 'Adviser',
+			user: "Adviser",
 			img: (
 				<>
-					<img src='https://cdn-icons.flaticon.com/png/512/3152/premium/3152908.png?token=exp=1645165417~hmac=71134b9dde204cc301ce379033a29c9b' />
+					<Image
+						src={adviser}
+						layout="fill"
+						objectFit="contain"
+						className={styles.illustration}
+					></Image>
 				</>
 			),
 		},
 		{
 			id: 3,
-			user: 'Moderator',
+			user: "Moderator",
 			img: (
 				<>
-					<img src='https://cdn-icons.flaticon.com/png/128/3152/premium/3152902.png?token=exp=1645165417~hmac=92f75c75295fd377768660d5dbbc8375' />
+					<Image
+						src={moderator}
+						layout="fill"
+						objectFit="contain"
+						className={styles.illustration}
+					></Image>
 				</>
 			),
 		},
@@ -46,28 +67,31 @@ function Register() {
 	// console.log(user);
 
 	const handleRegister = async (type) => {
-		console.log(type == 'researcher');
+		console.log(type == "researcher");
 		var data = new FormData();
-		data.append('type', type);
-		const response = await fetch(process.env.BACKEND_API_UR + `/users/${user?.id}/`, {
-			method: 'PATCH',
-			headers: {
-				Authorization: `Bearer ${Cookies.get('access_token')}`,
-			},
-			body: data,
-		});
+		data.append("type", type);
+		const response = await fetch(
+			process.env.BACKEND_API_UR + `/users/${user?.id}/`,
+			{
+				method: "PATCH",
+				headers: {
+					Authorization: `Bearer ${Cookies.get("access_token")}`,
+				},
+				body: data,
+			}
+		);
 		const result = await response.json();
 		// const response = await genericReq(`/users/${user?.id}/`, 'patch', 'withAuthMedia', data);
 		console.log(result);
 		const userType = result.type;
 		switch (userType) {
-			case 'researcher':
+			case "researcher":
 				router.push(`/workspaces?user=${user?.id}`);
 				break;
-			case 'adviser':
+			case "adviser":
 				router.push(`/classrooms?user=${user?.id}`);
 				break;
-			case 'moderator':
+			case "moderator":
 				router.push(`/institutions?user=${user?.id}`);
 				break;
 
@@ -78,25 +102,43 @@ function Register() {
 
 	return (
 		<>
-			<NextSeo title='Check User' />
-			<div className='h-screen p-5 flex flex-col items-center justify-center'>
-				<p className='text-4xl text-center font-bold text-gray-500'>
-					What type of user are you ?
-				</p>
+			<NextSeo title="Check User" />
+			<div className={styles.container}>
+				<p className={styles.question}>What type of user are you ?</p>
 
-				<div className='mainParent'>
+				<div className={styles.userContainer}>
 					{userTypes.map((item) => (
 						<div
 							key={item.id}
-							className='userCard'
+							className={styles.userCard}
 							onClick={() => handleRegister(item.user.toLowerCase())}
 						>
-							<div className='userCardImg'>{item.img}</div>
-							<p className='text-center mt-5'>{item.user}</p>
+							{/* <div> */}
+							<div className={styles.userCardImg}>{item.img}</div>
+							{/* </div> */}
+							<p className={styles.cardRole}>{item.user}</p>
 						</div>
 					))}
 				</div>
 			</div>
+			{/* <div className="h-screen p-5 flex flex-col items-center justify-center">
+				<p className="text-4xl text-center font-bold text-gray-500">
+					What type of user are you ?
+				</p>
+
+				<div className={styles.userContainer}>
+					{userTypes.map((item) => (
+						<div
+							key={item.id}
+							className="userCard"
+							onClick={() => handleRegister(item.user.toLowerCase())}
+						>
+							<div className="userCardImg">{item.img}</div>
+							<p className="text-center mt-5">{item.user}</p>
+						</div>
+					))}
+				</div>
+			</div> */}
 		</>
 	);
 }
