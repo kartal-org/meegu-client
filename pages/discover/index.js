1c6e23613e08dd03311a812f613a64d7eba0b735
nathanmaru <nathanmaru7@gmail.com>
2022-03-09 05:54:49 +0800

    discover premature

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -1,8 +1,36 @@
+import { TextField } from '@mui/material';
+import { useRouter } from 'next/router';
 import React from 'react';
+import ChipList from '../../components/reusable/chips';
 import PageLayout from '../../layouts/pageLayout';
+import styles from './discover.module.scss';
+import InputAdornment from '@mui/material/InputAdornment';
 
 function index() {
-	return <div>Discover</div>;
+	const router = useRouter();
+	return (
+		<div className={styles.page}>
+			<div className={styles.page__header}>
+				<h1>Discover</h1>
+				<input type='search' placeholder='Search Something...' />
+			</div>
+			<div className={styles.page__content}>
+				<ChipList
+					defaultVal={router.query.tab ? router.query.tab : 'all'}
+					chips={[
+						{ label: 'All', value: 'all', route: '/discover' },
+						{ label: 'Articles', value: 'article', route: '/discover?tab=article' },
+						{ label: 'People', value: 'people', route: '/discover?tab=people' },
+						{
+							label: 'Institutions',
+							value: 'institution',
+							route: '/discover?tab=institution',
+						},
+					]}
+				/>
+			</div>
+		</div>
+	);
 }
 
 index.Layout = PageLayout;
